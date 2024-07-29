db1b7f721167eb90d77ecff05ec3b0aca4c2626a
곽민우 <g.kmwdev@gmail.com>
2024-07-29 09:48:45 +0900

    Feat: 기본 사용법 추가 (#1)

--- easy-permission-aos-example/src/main/java/kr/co/kmwdev/easy_permission_aos/MainActivity.kt
+++ easy-permission-aos-example/src/main/java/kr/co/kmwdev/easy_permission_aos/MainActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.kmwdev.easy_permission_aos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,6 +16,7 @@ import kr.co.kmwdev.easy_permission_aos.ui.theme.EasypermissionaosTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             EasypermissionaosTheme {
                 // A surface container using the 'background' color from the theme
@@ -23,6 +25,29 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        // 앱 시작시 퍼미션 런처 등록
+        EasyPermission.registerPermissionLauncher(this)
+
+        EasyPermission.launchPermissions(
+            arrayOf(
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+        ) { permissions ->
+            permissions.forEach { p ->
+                if (p.value) {
+                    Log.d("EasyPermission", "Permission Granted: ${p.key}")
+                } else {
+                    Log.e("EasyPermission", "Permission Denied: ${p.key}")
                 }
             }
         }
